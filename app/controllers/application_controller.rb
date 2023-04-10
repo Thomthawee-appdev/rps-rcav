77d354049344c849258de7fed4aad70e77c20330
Thomthawee Leelahalamlert <thomthawee@uchicago.edu>
2023-04-10 19:12:32 +0000

    First Commit (Learning)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write a ton of code
+
+    #redirect_to("https://www.wikipedia.org")
+    render({ :html => "<h1>Howdy, world!</h1>".html_safe })
+  end
+
 end
